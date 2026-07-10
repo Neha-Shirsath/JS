f9c40988655cf7ff00730a05272fda3f40e15200
Neha Shirsath <nehashirsath308@gmail.com>
2026-07-10 17:00:51 +0000

    conversionOperation

--- .devcontainer/js_1/03_conversionOperation.js
+++ .devcontainer/js_1/03_conversionOperation.js
@@ -27,3 +27,36 @@ console.log(typeof(booleanLoogedIn));
 //"neha" => Boolean() => true
 //0 => Boolean() => false
 //"" => Boolean() => false
+
+// *************Operations******************
+
+let value = 3
+let negValue = -value
+
+console.log(negValue);
+
+console.log(3+2);
+console.log(3-2);
+console.log(3*2);
+console.log(3**2);
+console.log(3/2);
+console.log(3%2);
+
+let str1 = "Hello"
+let str2 = " Neha"
+console.log(str1+str2)
+
+console.log("1" + 2)//12
+console.log(1 + "2")//12
+console.log("1" + "2")//12
+console.log("1" + 2 + 2)//122
+console.log(1 + 2 + "2")//32
+
+let val1 = 50
+++val1;
+console.log(val1)//1 + 50 => prefix
+
+let val2 = 50
+val2++;
+console.log(val2)//50 + 1 => postfix
+
