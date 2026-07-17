6b7d2f120fdfe2e5f9cc93d735b97c653c3578d9
Neha Shirsath <nehashirsath308@gmail.com>
2026-07-17 16:10:41 +0000

    scope

--- 03_js_basics/02_scopes.js
+++ 03_js_basics/02_scopes.js
@@ -5,11 +5,42 @@ if(true){
     const b = 5;
     var c = 8
     console.log("Local scope: ",a);
-    
 }
-
 // console.log(a);//not accessible ouside scope
 // console.log(b);//not accessible ouside scope
 console.log(c);//accessible ouside scope and can redefined => not used
 
 console.log("Global scope: ",a);//400
+
+
+//closure
+function one(){
+    const username = "Neha";
+
+    function two(){
+        const platform = "Youtube";
+        console.log(username)//Neha => child func can access parent func
+    }
+    // console.log(platform)//not accessible outside func
+    console.log(username)
+    two()
+}
+// console.log(platform);
+// console.log(username);
+// one()
+
+
+//one way of function declaration
+console.log(firstWay(4));//5 => accessible
+function firstWay(num){
+    return num + 1;
+} 
+// console.log(firstWay(4));
+
+
+//another way of function declaration => function in variable
+// console.log(secondWay(9));//cannt access => error
+const secondWay = function(num){
+    return num + 1;
+} 
+console.log(secondWay(9));
