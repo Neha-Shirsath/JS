5df6c60c62b5d6123faef61f2dae3b2289daf9f1
Neha Shirsath <nehashirsath308@gmail.com>
2026-07-17 04:28:35 +0000

    functions

--- 03_js_basics/01_functions.js
+++ 03_js_basics/01_functions.js
@@ -46,3 +46,35 @@ function userLoggedIn(username = "user"){//default val user
 }
 console.log(userLoggedIn("neha30"));
 console.log(userLoggedIn());//undefined just logged in
+
+function numbers(val1, val2, ...nums){
+    //...nums => REST operator
+    return nums;
+}
+console.log(numbers(200, 400, 5000, 10));//val1 => 200, val2 => 400, nums =[5000, 10]
+
+//obj in functions
+const user = {
+    name: "Neha",
+    price: 199
+}
+
+function handleObj(anyobject){
+    console.log(`Username is ${anyobject.name} and price is ${anyobject.price}`)   
+}
+//handleObj(user)
+handleObj({
+    name: "Neha",
+    price: 199
+})
+
+//array in functions
+const newArray = [300, 600, 800]
+
+function handleArray(anyArray){
+    // return anyArray;
+    return anyArray[2];
+}
+// console.log(handleArray(newArray));
+console.log(handleArray([2, 8, 12]));
+
