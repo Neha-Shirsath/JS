da93da31dc1d32d861fdb04f4f1340099f9f2569
Neha Shirsath <nehashirsath308@gmail.com>
2026-07-23 13:54:02 +0000

    filter

--- 05_iterations/05_filter.js
+++ 05_iterations/05_filter.js
@@ -7,7 +7,7 @@
 // console.log(values);//undefined (foreach doesnt return vals)
 
 
-//filter
+//filter => condition on elements
 const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 // const values = myArray.filter((num) => num > 4)
