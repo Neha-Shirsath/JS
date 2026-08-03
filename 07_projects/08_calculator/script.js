f3e6415c40028ff614d08d4cacd91ba608640ff8
Neha Shirsath <nehashirsath308@gmail.com>
2026-08-03 19:04:17 +0530

    commit

---
+++ 07_projects/08_calculator/script.js
@@ -0,0 +1,29 @@
+let input = document.getElementById("input");
+let buttons = document.querySelectorAll("button");
+
+let str = "";
+let btnArr = Array.from(buttons)
+
+btnArr.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        if(e.target.innerHTML === "="){
+            str = eval(str);
+            input.value = str;
+        }
+
+        else if(e.target.innerHTML == "AC"){
+            str = "";
+            input.value = str;
+        }
+
+        else if(e.target.innerHTML == "DEL"){
+            str = str.substring(0, str.length-1);
+            input.value = str;
+        }
+
+        else{
+            str += e.target.innerHTML;
+            input.value = str;
+        }
+    });
+});
